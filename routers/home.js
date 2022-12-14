499c0374401ba21729410bc392b77537c36e1283
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-12-14 14:27:42 -0600

    fixed exports and created post

--- routers/home.js
+++ routers/home.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 const app = Router();
 
-export default app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.set("Content-Type", "text/plain");
   res.status(200);
   res.send("Welcome the todo api");
 });
+
+export { app as default };
